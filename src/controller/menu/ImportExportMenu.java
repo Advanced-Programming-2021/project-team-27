7f2227f859a4694ef18ad179627a688b876eb51b
AliMarshmallow <alirahimyakbar@gmail.com>
2021-04-16 21:12:32 +0430

    1.00

--- src/controller/menu/ImportExportMenu.java
+++ src/controller/menu/ImportExportMenu.java
@@ -5,6 +5,7 @@ import java.util.regex.Matcher;
 public class ImportExportMenu {
 
     public void importCard(String cardName) {
+        int checker;
     }
 
     public void exportCard(String cardName) {
