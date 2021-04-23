cff3242c2c8651a79bd2a3faa5254ef1426f3751
aradmaleki02 <arad.m.t02@gmail.com>
2021-04-23 16:16:48 +0430

    akbar

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import view.menu.LoginMenuView;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("akbar");
         LoginMenuView loginMenuView = new LoginMenuView();
         loginMenuView.run();
     }
