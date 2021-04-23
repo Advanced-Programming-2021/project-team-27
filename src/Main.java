d4d03273c1991c2cfc8bba518b3e21bffec0814b
aradmaleki02 <arad.m.t02@gmail.com>
2021-04-23 16:20:44 +0430

    not akbar

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import view.menu.LoginMenuView;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("akbar");
         LoginMenuView loginMenuView = new LoginMenuView();
         loginMenuView.run();
     }
