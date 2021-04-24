ae0d363470d6857538b30a96d6639ba60f963707
Amirreza Ghadyani <amirreza8010@gmail.com>
2021-04-24 11:42:41 +0430

    read csv files and complete access to cards data

--- src/model/card/Card.java
+++ src/model/card/Card.java
@@ -3,9 +3,9 @@ package model.card;
 import java.util.ArrayList;
 
 public class Card {
-    private static ArrayList<Card> cards=new ArrayList<>();
+    private static ArrayList<Card> cards = new ArrayList<>();
     protected String name;
-    protected String type;
+    protected String cardType;
     protected String description;
     protected String id;
     protected int price;
@@ -22,17 +22,21 @@ public class Card {
         return name;
     }
 
-    public static ArrayList<Card> getAllCards(){
+    public static ArrayList<Card> getAllCards() {
         return null;
     }
 
 
-    public static Card getCardByName(String name){
+    public static Card getCardByName(String name) {
         for (Card card : cards) {
             if (card.getName().equals(name))
                 return card;
         }
         return null;
+    }
+
+    public static void addToCards(Card card) {
+        cards.add(card);
     }
 
 }
--- src/model/card/Trap.java
+++ src/model/card/Trap.java
@@ -1,5 +1,22 @@
 package model.card;
 
+import java.util.ArrayList;
+
 public class Trap extends Card{
 
+    protected ArrayList<Trap> allTraps = new ArrayList<>();
+
+    protected String icon;
+    protected String status;
+
+
+    public Trap(String name, String icon, String description, String status, int price) {
+        this.name = name;
+        this.icon = icon;
+        this.description = description;
+        this.status = status;
+        this.price = price;
+        allTraps.add(this);
+        Card.addToCards(this);
+    }
 }
