e2e17cdcd998d63d5ff93c9f72eb84469289c9aa
Amirreza Ghadyani <77363478+amirrezaghadyani@users.noreply.github.com>
2021-05-05 05:52:01 +0430

    profile menu compressed version

--- src/view/menu/ProfileMenuView.java
+++ src/view/menu/ProfileMenuView.java
@@ -5,7 +5,6 @@ import view.CommandMatcher;
 import view.ScanInput;
 import view.TerminalOutput;
 
-import java.net.CookieHandler;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,10 +12,11 @@ public class ProfileMenuView {
 
     private String currentUsername;
     private ProfileMenu profileMenu;
+    private boolean isChangePasswordInputCompressed = true;
 
     public ProfileMenuView(String currentUsername) {
         setCurrentUsername(currentUsername);
-        profileMenu=new ProfileMenu(currentUsername);
+        profileMenu = new ProfileMenu(currentUsername);
     }
 
     public void setCurrentUsername(String currentUsername) {
@@ -30,9 +30,9 @@ public class ProfileMenuView {
             if (input.matches("menu exit")) {
                 TerminalOutput.output("exit successfully!");
                 break;
-            } else if (input.matches("profile change --nickname ([^ ]+)")) {
+            } else if (input.matches("profile change (--nickname|-n) ([^ ]+)")) {
                 profileChangeNickname(input);
-            } else if (isInputChangePasswordValid(input)) {
+            } else if (isChangePasswordInputValid(input)) {
                 profileChangePassword(input);
             } else if (input.matches("menu show-current")) {
                 TerminalOutput.output("Profile Menu");
@@ -45,30 +45,62 @@ public class ProfileMenuView {
     }
 
     public void profileChangeNickname(String input) {
-        Matcher matcher = CommandMatcher.getCommandMatcher(input, "profile change --nickname ([^ ]+)");
-        String nickname = matcher.group(1);
+        Matcher matcher = CommandMatcher.getCommandMatcher(input, "profile change (--nickname|-n) ([^ ]+)");
+        String nickname = matcher.group(2);
         profileMenu.profileChangeNickname(nickname);
     }
 
     public void profileChangePassword(String input) {
-        Matcher matcherForNewPassword=CommandMatcher.getCommandMatcher(input,"--new ([^ ]+)");
-        String newPassword=matcherForNewPassword.group(1);
-        Matcher matcherForCurrentPassword= CommandMatcher.getCommandMatcher(input,"--current ([^ ]+)");
-        String currentPassword=matcherForCurrentPassword.group(1);
-        profileMenu.profileChangePassword(currentPassword,newPassword);
+        String newPassword;
+        String currentPassword;
+        if (isChangePasswordInputCompressed) {
+            Matcher matcherForNewPassword = CommandMatcher.getCommandMatcher(input, "-n ([^ ]+)");
+            newPassword = matcherForNewPassword.group(1);
+            Matcher matcherForCurrentPassword = CommandMatcher.getCommandMatcher(input, "-c ([^ ]+)");
+            currentPassword = matcherForCurrentPassword.group(1);
+        } else {
+            Matcher matcherForNewPassword = CommandMatcher.getCommandMatcher(input, "--new ([^ ]+)");
+            newPassword = matcherForNewPassword.group(1);
+            Matcher matcherForCurrentPassword = CommandMatcher.getCommandMatcher(input, "--current ([^ ]+)");
+            currentPassword = matcherForCurrentPassword.group(1);
+            setChangePasswordInputCompressed(true);
+        }
+        profileMenu.profileChangePassword(currentPassword, newPassword);
     }
 
-    public boolean isInputChangePasswordValid(String input){
-        if (CommandMatcher.getCommandMatcher(input,"profile change (.+)") != null){
-            if (CommandMatcher.getCommandMatcher(input,"--password") != null){
-                if (CommandMatcher.getCommandMatcher(input,"--current [^ -]+") !=null){
-                    if (CommandMatcher.getCommandMatcher(input,"--new [^ -]+")!=null)
-                        return true;
+    public boolean isChangePasswordInputValid(String changePasswordInput) {
+        if (changePasswordInput.matches("^profile change (.+)")) {
+            if (changePasswordInput.split(" ").length == 7) {
+                if (isThisRegexExistInInput(changePasswordInput, "(--current [^ ]+)")) {
+                    if (isThisRegexExistInInput(changePasswordInput, "(--password )")) {
+                        if (isThisRegexExistInInput(changePasswordInput, "(--new [^ ]+)")) {
+                            setChangePasswordInputCompressed(false);
+                            return true;
+                        }
+                    }
+                }
+                if (isThisRegexExistInInput(changePasswordInput, "(-c [^ ]+)")) {
+                    if (isThisRegexExistInInput(changePasswordInput, "(-p [^ ]+)")) {
+                        return isThisRegexExistInInput(changePasswordInput, "(-n [^ ]+)");
+                    }
                 }
             }
         }
         return false;
     }
 
+    private static boolean isThisRegexExistInInput(String input, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
 
+
+    public boolean isChangePasswordInputCompressed() {
+        return isChangePasswordInputCompressed;
+    }
+
+    public void setChangePasswordInputCompressed(boolean changePasswordInputCompressed) {
+        isChangePasswordInputCompressed = changePasswordInputCompressed;
+    }
 }
